fd5356083d67a13db1dedda365e8abaee9268c79
Cedric van Putten <me@bycedric.com>
2021-01-18 15:46:35 +0100

    feature: add ejected app version

---
+++ apps/ejected/App.tsx
@@ -0,0 +1,24 @@
+import { Paragraph, Strong } from "@acme/ui";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Paragraph>
+        Hello from an <Strong>EAS</Strong> monorepo (ejected)
+      </Paragraph>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
