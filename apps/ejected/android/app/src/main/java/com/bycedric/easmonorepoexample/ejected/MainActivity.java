1bf0da2646c25605355c48ea7c3c5c59aab7aba4
Cedric van Putten <me@bycedric.com>
2021-07-05 14:32:21 +0200

    chore: upgrade to expo sdk 42 (#11)
    * chore: upgrade all package dependencies
    
    * chore: upgrade managed app to sdk 42
    
    * chore: upgrade ejected app to sdk 42
    
    * chore: regenerate native files for ejected with prebuild

--- apps/ejected/android/app/src/main/java/com/bycedric/easmonorepoexample/ejected/MainActivity.java
+++ apps/ejected/android/app/src/main/java/com/bycedric/easmonorepoexample/ejected/MainActivity.java
@@ -1,4 +1,6 @@
 package com.bycedric.easmonorepoexample.ejected;
+import android.content.res.Configuration;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -10,13 +12,30 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.splashscreen.singletons.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
+
 public class MainActivity extends ReactActivity {
+
+    // Added automatically by Expo Config
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        sendBroadcast(intent);
+    }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    // Set the theme to AppTheme BEFORE onCreate to support 
+    // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
+    setTheme(R.style.AppTheme);
+    super.onCreate(null);
+// @generated begin expo-splash-screen-mainActivity-onCreate-show-splash - expo prebuild (DO NOT MODIFY) sync-8915a20732e7fda227585f9b6ef0d38bef4fbbbe
+    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
+// @generated end expo-splash-screen-mainActivity-onCreate-show-splash
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
   }
 
 
