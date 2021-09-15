3b0d4ff6d81d228d811a906adefd6b2bf38da35f
Cedric van Putten <me@bycedric.com>
2021-09-15 13:21:24 +0200

    refactor: move managed to new monorepo config for sdk 43

--- apps/managed/metro.config.js
+++ apps/managed/metro.config.js
@@ -1,3 +1,38 @@
-const { createMetroConfiguration } = require("expo-yarn-workspaces");
+const { getDefaultConfig } = require("@expo/metro-config");
+const findUp = require("find-up");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = createMetroConfiguration(__dirname);
+const projectPath = __dirname;
+const workspacePath = getWorkspaceRoot(__dirname);
+
+const config = getDefaultConfig(projectPath);
+
+// We want to watch all folders in the monorepo, instead of just `apps/managed`
+config.watchFolders = [workspacePath];
+// Modules can be hoisted to root or installed locally, resolve in this order.
+config.resolver.nodeModulesPaths = [
+  path.join(projectPath, "node_modules"),
+  path.join(workspacePath, "node_modules"),
+];
+
+module.exports = config;
+
+function getWorkspaceRoot(cwd) {
+  return findUp.sync(
+    (directory) => {
+      const filePath = path.join(directory, "package.json");
+      const fileContent = fs.existsSync(filePath)
+        ? fs.readFileSync(filePath, "utf-8")
+        : null;
+
+      if (fileContent && Array.isArray(JSON.parse(fileContent).workspaces)) {
+        return directory;
+      }
+    },
+    {
+      type: "directory",
+      cwd: path.join(cwd, ".."),
+    }
+  );
+}
