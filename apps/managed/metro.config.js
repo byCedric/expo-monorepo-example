25b79251897306d1df6ffd7282b58a1196ac488b
Cedric van Putten <me@bycedric.com>
2021-10-07 21:43:30 +0200

    refactor: remove expo-yarn-workspace from the repository

--- apps/managed/metro.config.js
+++ apps/managed/metro.config.js
@@ -1,3 +1,4 @@
-const { createMetroConfiguration } = require("expo-yarn-workspaces");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = createMetroConfiguration(__dirname);
+module.exports = getDefaultConfig(__dirname);
