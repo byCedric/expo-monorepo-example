266ec277e7b2c56d60a8c967d79695b2c2aef117
Cedric van Putten <me@bycedric.com>
2021-10-11 19:16:55 +0200

    refactor: move metro config out to its own package

--- apps/with-sentry/metro.config.js
+++ apps/with-sentry/metro.config.js
@@ -1,5 +1,5 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { withMonorepo } = require("@acme/metro-config");
+const { withMonorepo } = require("expo-monorepo");
 const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = withMonorepo(getDefaultConfig(__dirname));
