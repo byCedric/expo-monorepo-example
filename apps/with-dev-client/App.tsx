b7fb81c2dc64a93c495bc7aad537d869fc0e6948
Renan Machado <renan.mav@hotmail.com>
2021-10-24 17:41:55 -0300

    feat: setup react-native-mmkv

--- apps/with-dev-client/App.tsx
+++ apps/with-dev-client/App.tsx
@@ -1,13 +1,34 @@
 import { Paragraph, Strong } from "@acme/ui";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import { MMKV, useMMKVString } from "react-native-mmkv";
+import React, { useState } from "react";
+
+const storage = new MMKV();
 
 export default function App() {
+  const [usernameInput, setUsernameInput] = useState("");
+
+  const [username, setUsername] = useMMKVString("user.name", storage);
+
   return (
     <View style={styles.container}>
       <Paragraph>
         Hello from an <Strong>EAS</Strong> monorepo (with-dev-client)
       </Paragraph>
+      <Paragraph>
+        Your MMKV username is <Strong>{username ?? "unknown"}</Strong>
+      </Paragraph>
+      <TextInput
+        placeholder="Type your username here"
+        value={usernameInput}
+        onChangeText={(u) => setUsernameInput(u)}
+        style={styles.input}
+      />
+      <Button
+        title="Save"
+        onPress={() => usernameInput.length && setUsername(usernameInput)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +40,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    fontSize: 20,
+    marginTop: 10,
   },
 });
