1e9cc924cf0e5896d11c78513e541bf34ae8391d
Cedric van Putten <me@bycedric.com>
2021-10-25 15:32:10 +0200

    chore: test changes

--- apps/ejected/App.tsx
+++ apps/ejected/App.tsx
@@ -13,6 +13,8 @@ export default function App() {
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- apps/managed/App.tsx
+++ apps/managed/App.tsx
@@ -13,6 +13,7 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
