9657e4eedea19b094220decc653264962d0ba070
Cedric van Putten <me@bycedric.com>
2021-11-03 11:57:26 +0100

    feature(apps/managed): add example code from ui kittens

--- apps/managed/App.tsx
+++ apps/managed/App.tsx
@@ -1,15 +1,20 @@
-import { Paragraph, Strong } from "@acme/ui";
+import { Strong } from "@acme/ui";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Paragraph>
-        Hello from an <Strong>EAS</Strong> monorepo (managed)
-      </Paragraph>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={styles.container}>
+        <Text category="h1">Hello!</Text>
+        <Text category="p1">
+          from an <Strong>EAS</Strong> monorepo (managed)
+        </Text>
+        <StatusBar style="auto" />
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
