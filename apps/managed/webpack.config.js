4c9e1c9ff44cefcdbb6b92f0fcee4efc370d109d
Cedric van Putten <me@bycedric.com>
2021-11-03 11:49:33 +0100

    fix(apps/managed): add ui kittens to transpile list in webpack

--- apps/managed/webpack.config.js
+++ apps/managed/webpack.config.js
@@ -1,7 +1,15 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ["@ui-kitten/components"],
+      },
+    },
+    argv
+  );
   // Customize the config before returning it.
   return config;
 };
