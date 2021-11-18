c501151ec96b62ad28dd1978fbbdecfed277536c
Cedric van Putten <me@bycedric.com>
2021-11-18 08:06:33 -0800

    chore: also build managed apps in the pr

--- apps/managed/index.js
+++ apps/managed/index.js
@@ -6,3 +6,5 @@ import App from "./App";
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+// this comment should be removed before merging
