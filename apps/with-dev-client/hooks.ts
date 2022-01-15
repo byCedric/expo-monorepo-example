c357e0c341eaa40ba3be931cd612a39bfc04e7f5
Cedric van Putten <me@bycedric.com>
2022-01-15 17:51:20 +0100

    chore: move global tsconfig to packages/ui and reuse expo ts config

--- apps/with-dev-client/hooks.ts
+++ apps/with-dev-client/hooks.ts
@@ -1,7 +1,7 @@
-import { useCallback, useState } from "react";
-import { MMKV } from "react-native-mmkv";
+import { useCallback, useState } from 'react';
+import { MMKV } from 'react-native-mmkv';
 
-type MmkvString = ReturnType<MMKV["getString"]>;
+type MmkvString = ReturnType<MMKV['getString']>;
 type MmkvStringSetter = (value?: string) => void;
 
 /**
@@ -9,10 +9,7 @@ type MmkvStringSetter = (value?: string) => void;
  * Note, this does not update when the value is changed without the setter.
  * This version (1.3.1) of MMKV does not have the value listener.
  */
-export function useMmkvString(
-  name: string,
-  storage: MMKV
-): [MmkvString, MmkvStringSetter] {
+export function useMmkvString(name: string, storage: MMKV): [MmkvString, MmkvStringSetter] {
   const [value, setValue] = useState<MmkvString>(() => storage.getString(name));
 
   const setCallback = useCallback<MmkvStringSetter>(
