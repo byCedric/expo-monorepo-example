e72391e13f16737bb96a3b656cae63c2fe81aa53
Cedric van Putten <me@bycedric.com>
2022-04-11 18:16:45 +0200

    chore: add dedicated app for eas updates

---
+++ apps/updates/metro.config.js
@@ -0,0 +1,16 @@
+// Learn more https://docs.expo.dev/guides/monorepos
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
