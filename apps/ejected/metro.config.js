01921d2814c7a0873f66721f9561fee62506678c
Michael Tichopad <michael@tichopad.dev>
2022-04-11 18:07:28 +0200

    fix: nodeModulesPaths typo in metro config (#48)

--- apps/ejected/metro.config.js
+++ apps/ejected/metro.config.js
@@ -8,7 +8,7 @@ const workspaceRoot = path.resolve(projectRoot, '../..');
 const config = getDefaultConfig(projectRoot);
 
 config.watchFolders = [workspaceRoot];
-config.resolver.nodeModulesPath = [
+config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ];
