71514ec42e22151240f30845a244dc6beb3ace79
Cedric van Putten <me@bycedric.com>
2023-01-11 16:33:18 +0100

    fix(eslint-config): ignore react native in import due to eslint not supporting it

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,4 +4,9 @@ module.exports = {
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
+  // Disable import/namespace due to https://github.com/facebook/react-native/issues/28549
+  // By setting delimiters to `\|/`, this ignore is supported on Windows too
+  settings: {
+    'import/ignore': ['node_modules(\\\\|/)react-native(\\\\|/)index\\.js$'],
+  },
 };
