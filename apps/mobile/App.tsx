9cd994b2a63af6ab183b1fcd8723ba3f8bcb6f57
Cedric van Putten <me@bycedric.com>
2023-01-13 23:49:52 +0100

    feature(mobile): add share testing code

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,22 @@
 import { HomeIcon, HomeScreen } from '@acme/feature-home';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+import Share from 'react-native-share';
 
 export default function App() {
+  async function testShare() {
+    try {
+      const result = await Share.open({ url: 'https://docs.expo.dev' });
+      console.log('Shared Expo docs', result);
+    } catch (error) {
+      console.log('Failed to share', error);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <HomeIcon style={{ fontSize: 64 }} />
+      <Button title="Test share" onPress={testShare} />
       <HomeScreen />
       <StatusBar style="auto" />
     </View>
