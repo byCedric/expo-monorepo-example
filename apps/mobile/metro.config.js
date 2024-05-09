35e890ee665387e4e606b16a385e6d5093feda97
Cedric van Putten <github@cedric.dev>
2024-05-09 14:17:08 +0200

    fix(mobile): re-enable hierarchical lookup

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -10,8 +10,6 @@ const config = getDefaultConfig(projectRoot);
 
 // #1 - Watch all files in the monorepo
 config.watchFolders = [workspaceRoot];
-// #3 - Force resolving nested modules to the folders below
-config.resolver.disableHierarchicalLookup = true;
 // #2 - Try resolving with project modules first, then workspace modules
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
