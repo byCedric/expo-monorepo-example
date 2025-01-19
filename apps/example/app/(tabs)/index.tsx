aa16df74c241fc4175ebed17e79de5c9e6ac2682
Cedric van Putten <github@cedric.dev>
2025-01-19 16:02:23 +0100

    refactor(example): drop step 3 as we removed the reset project script

--- apps/example/app/(tabs)/index.tsx
+++ apps/example/app/(tabs)/index.tsx
@@ -45,16 +45,6 @@ export default function HomeScreen() {
           Tap the Explore tab to learn more about what's included in this starter app.
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
     </ParallaxScrollView>
   );
 }
