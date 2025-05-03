503cf33af8a1bbeba292908957393dc9efdb02b6
Cedric van Putten <github@cedric.dev>
2025-05-03 15:04:19 +0200

    refactor: swap out `react-test-renderer` for `@testing-library/react-native`

--- apps/example/components/__tests__/ThemedText-test.tsx
+++ apps/example/components/__tests__/ThemedText-test.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 
 import { ThemedText } from '../ThemedText';
 
 it(`renders correctly`, () => {
-  const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
+  const component = render(<ThemedText>Snapshot test!</ThemedText>);
+  expect(component.toJSON()).toMatchSnapshot();
 });
