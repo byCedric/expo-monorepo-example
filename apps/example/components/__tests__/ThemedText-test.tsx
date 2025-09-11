c5be82a57da62ab45ba4670a65cf40b57dc57105
Cedric van Putten <me@bycedric.com>
2025-09-11 20:08:52 +0200

    refactor: upgrade to Expo SDK 54 (#148)
    * refactor: upgrade to Expo SDK 54 beta
    
    * fix(example): adjust some versions for 0.81.0
    
    * refactor: regenerate example app and update to Expo SDK 54 stable
    
    * chore(example): resolve all linting issues
    
    * chore(example): add missing `expo-splash-screen` back
    
    * chore: break pnpm lockfile to trigger `react-native-worklets@0.5.1` upgrade

--- apps/example/components/__tests__/ThemedText-test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react-native';
-
-import { ThemedText } from '../ThemedText';
-
-it(`renders correctly`, () => {
-  const component = render(<ThemedText>Snapshot test!</ThemedText>);
-  expect(component.toJSON()).toMatchSnapshot();
-});
--- apps/example/metro.config.js
+++ apps/example/metro.config.js
@@ -1,23 +1,10 @@
 // Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require('expo/metro-config');
-const { FileStore } = require('metro-cache');
-const path = require('path');
+const { FileStore } = require('@expo/metro/metro-cache');
+const path = require('node:path');
 
 // Create the default Expo config for Metro
-// This includes the automatic monorepo configuration for workspaces
-// See: https://docs.expo.dev/guides/monorepos/#automatic-configuration
 const config = getDefaultConfig(__dirname);
-
-// You can configure it manually as well, the most important parts are:
-// const projectRoot = __dirname;
-// const workspaceRoot = path.join(__dirname, '..', '..');
-// #1 - Watch all files within the monorepo
-// config.watchFolders = [workspaceRoot];
-// #2 - Try resolving with project modules first, then hoisted workspace modules
-// config.resolver.nodeModulesPaths = [
-//   path.resolve(projectRoot, 'node_modules'),
-//   path.resolve(workspaceRoot, 'node_modules'),
-// ];
 
 // Use turborepo to restore the cache when possible
 config.cacheStores = [
