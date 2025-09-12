4dd526c9bb09d79396d19fb19c6e2d2a4c423e98
Cedric van Putten <github@cedric.dev>
2025-09-12 15:26:59 +0200

    docs(example): add link to PR to fix the jest issue

--- apps/example/jest.config.js
+++ apps/example/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   ...config,
   // The transform ignore patterns from `jest-expo` are broken for pnpm store paths.
   // All it needs is an ending `/` to make it work, unclear why though.
+  // See: https://github.com/expo/expo/pull/39605
   transformIgnorePatterns: config.transformIgnorePatterns.map((pattern) =>
     pattern.endsWith('/') ? pattern : `${pattern}/`
   ),
